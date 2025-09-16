219543a2fdc5d81ae7ee91decf050e682d06300d
Tin <tinptse182207@fpt.edu.vn>
2025-09-16 15:47:16 +0700

    update

--- Admin-SpeedyGame/vite.config.js
+++ Admin-SpeedyGame/vite.config.js
@@ -4,6 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  base: '/',
   server: {
     proxy: {
       '/api': {
@@ -23,5 +24,8 @@ export default defineConfig({
         },
       },
     },
+    build: {
+    outDir: 'dist'
+  }
   },
 });
