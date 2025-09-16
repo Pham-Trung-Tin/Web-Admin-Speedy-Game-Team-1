15f522390bbbde64fe42bb6b38095a0fc9ffc861
Tin <tinptse182207@fpt.edu.vn>
2025-09-16 16:00:26 +0700

    .

--- Admin-SpeedyGame/src/page/Admin/profileAdmin/Profile.jsx
+++ Admin-SpeedyGame/src/page/Admin/profileAdmin/Profile.jsx
@@ -1,6 +1,6 @@
 import { useState, useEffect } from "react";
 import { useNavigate } from "react-router-dom";
-import { AuthService } from "../../../services/AuthService";
+import { AuthService } from "../../../services/AuthService.js";
 import GameHistory from "../gameHistory/GameHistory";
 import GameRoomUser from "../gameRoomUser/GameRoomUser";
 import "./Profile.css";
